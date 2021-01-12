9033065cfd71a0c1b9f42fe41680e60180c17683
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2021-01-11 22:10:31 -0500

    Update text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,3 @@
-/********************************************************************************* *
-*  WEB422 – Assignment 06
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this 
-* assignment has been copied manually or electronically from any other source (including web sites) or 
-* distributed to other students. 
-* 
-* Name: Yoonkyung Kim  Student ID: 121389191  Date: 12.10.2020 
-* 
-* Online Link: https://first-angular-blog.netlify.app/
-********************************************************************************/
 import { Component } from '@angular/core';
 
 @Component({
